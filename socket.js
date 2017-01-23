a21d5ff9f956a4ec7a16181b5ccb115611437e2c
krokodiili <laurimeltti@gmail.com>
2017-01-23 22:18:34 +0200

    -

--- socket.js
+++ socket.js
@@ -9,7 +9,7 @@ let counter = 1;
 let videoCount = 0;
 let timer = null;
 let currentVideoTime = 0;
-let currentVideo = "MuMEwVPwNVU";
+let currentVideo = "PxWYNqeWqyY";
 
 module.exports.listen = function (http) {
 //connection
@@ -93,7 +93,7 @@ module.exports.listen = function (http) {
             skipCount = 0;
             console.log(videoQueue + "!!!!!!");
             if (videoQueue.length < 1) {
-                io.emit("next video", "MuMEwVPwNVU", skipCount, false, videoCount);
+                io.emit("next video", "PxWYNqeWqyY", skipCount, false, videoCount);
                 currentVideoTime = 0;
                 counter = 1;
             } else {
